3753093dc88fcaed5cab69278d0aed19157f615a
pauloli <616752336@qq.com>
2022-09-15 01:03:11 +0800

    feat: navItems

---
+++ pages/created-nfts.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const CreatedNFTs = () => {
+  console.log('create');
+  return (
+    <div>CreatedNFTs</div>
+  );
+};
+
+export default CreatedNFTs;
---
+++ pages/my-nfts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyNFTs = () => {
+  console.log('my');
+
+  return (
+    <div>MyNFTs</div>
+  );
+};
+
+export default MyNFTs;
